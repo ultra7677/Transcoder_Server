2e6f2213e61fa2678d07cf62ddca1757308737d2
ultra <ultra7677@gmail.com>
2017-05-01 16:25:10 +0800

    before fix the blocking part

--- server.cpp
+++ server.cpp
@@ -10,88 +10,100 @@ extern "C"{
 }
 #include "transcoder_core.h"
 #define BUFFSIZE 1024
- 
+
 void * get_in_addr(struct sockaddr * sa)
 {
-	if(sa->sa_family == AF_INET)
-	{
-		return &(((struct sockaddr_in *)sa)->sin_addr);
-	}
+    if(sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in *)sa)->sin_addr);
+    }
 
-	return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
-
-int main( int argc, char **argv)  
+void init_tcp_server()
 {
-// Variables for writing a server.
+    // Variables for writing a server.
     int status;
     struct addrinfo hints, * res;
     int listner;
-// Before using hint you have to make sure that the data structure is empty
+
+    // Before using hint you have to make sure that the data structure is empty
     memset(& hints, 0, sizeof hints);
-// Set the attribute for hint
+    
+    // Set the attribute for hint
     hints.ai_family = AF_UNSPEC; // Dont care V4 or V6
     hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
     hints.ai_flags = AI_PASSIVE;    // Fill in my IP for me 
-// Fill the res data structure and make sure that the results make sense.
+
+    // Fill the res data structure and make sure that the results make sense.
     status = getaddrinfo(NULL, "8888" , &hints, &res);
     if(status != 0)
     {
-	fprintf(stderr,"getaddrinfo error: %s\n",gai_strerror(status));
+        fprintf(stderr,"getaddrinfo error: %s\n",gai_strerror(status));
     }
-// Create Socket and check if error occured afterwards
+    
+    // Create Socket and check if error occured afterwards
     listner = socket(res->ai_family,res->ai_socktype, res->ai_protocol);
     if(listner < 0 )
     {
-	fprintf(stderr,"socket error: %s\n",gai_strerror(status));
+        fprintf(stderr,"socket error: %s\n",gai_strerror(status));
     }
-// Bind the socket to the address of my local machine and port number
+    
+    // Bind the socket to the address of my local machine and port number
     status = bind(listner, res->ai_addr, res->ai_addrlen);
     if(status < 0)
     {
-	fprintf(stderr,"bind: %s\n",gai_strerror(status));
+        fprintf(stderr,"bind: %s\n",gai_strerror(status));
     }
 
     status = listen(listner, 10);
     if(status < 0)
     {
-	fprintf(stderr,"listen: %s\n",gai_strerror(status));
+        fprintf(stderr,"listen: %s\n",gai_strerror(status));
     }
-// Free the res linked list after we are done with it
+
+    // Free the res linked list after we are done with it
     freeaddrinfo(res);
-// We should wait now for a connection to accept
-	int new_conn_fd;
-	struct sockaddr_storage client_addr;
-	socklen_t addr_size;
-	char s[INET6_ADDRSTRLEN]; // an empty string
-        char buf[BUFFSIZE];
-	// Calculate the size of the data structure
-	addr_size = sizeof client_addr;
 
-	printf("I am now accepting connections ...\n");
+    // We should wait now for a connection to accept
+    int new_conn_fd;
+    struct sockaddr_storage client_addr;
+    socklen_t addr_size;
+    char s[INET6_ADDRSTRLEN]; // an empty string
+    char buf[BUFFSIZE];
+    // Calculate the size of the data structure
+    addr_size = sizeof client_addr;
+    printf("I am now accepting connections ...\n");
 
-	while(1){
-		// Accept a new connection and return back the socket desciptor
-		new_conn_fd = accept(listner, (struct sockaddr *) & client_addr, &addr_size);
-		if(new_conn_fd < 0)
-		{
-			fprintf(stderr,"accept: %s\n",gai_strerror(new_conn_fd));
-			continue;
-		}
-		inet_ntop(client_addr.ss_family, get_in_addr((struct sockaddr *) &client_addr),s ,sizeof s);
-		printf("I am now connected to %s \n",s);
+    while(1){
+        // Accept a new connection and return back the socket desciptor
+        new_conn_fd = accept(listner, (struct sockaddr *) & client_addr, &addr_size);
+        if(new_conn_fd < 0)
+        {
+            fprintf(stderr,"accept: %s\n",gai_strerror(new_conn_fd));
+            continue;
+        }
+        inet_ntop(client_addr.ss_family, get_in_addr((struct sockaddr *) &client_addr),s ,sizeof s);
+        printf("I am now connected to %s \n",s);
 
         int received = -1;
         int total = 0;
         int i = 0;
-        int first_flag = 0;
+        int is_first_call = 1;
         char *data = (char*)malloc(10000000);
         char tmp[BUFFSIZE];
-
+        /*
+         * The code below is for receiving data from tcp client.
+         * recv() will be called several times when the command is postVideo, 
+         * and the tcp client will close connection after sending all the data.
+         * For other commands, recv() will be called only once, and will break after first call.
+         */ 
         while((received = recv(new_conn_fd, buf, BUFFSIZE, 0)) > 0){
+
             if (received == -1)  printf("Failed to receive initial bytes from client");
-            if (received == 0) break;
+            if (received == 0)   break; 
+
             int j = 0;
             while(j < received){
                 data[i+j] = buf[j];
@@ -100,22 +112,23 @@ int main( int argc, char **argv)
             i += received;
             total += received;            
            // printf("data : %s\n", data);
-            if (received != 1024 && first_flag == 0){
+            if (received != 1024 && is_first_call == 1){
                 json_object * jobj = json_tokener_parse(data);
                 if (strcmp(json_object_get_string((json_object_object_get(jobj,"command"))),"postVideo") != 0) break;
             }
-            first_flag = 1; 	 
+            is_first_call = 0;      
         }
         
-        printf("received and saved total of %zu bytes\n", total );
-       // printf("Received String from Client : %s\n",data);
-        printf("Length of data : %d\n", strlen(data));
+        //printf("received and saved total of %zu bytes\n", total );
+        //printf("Received String from Client : %s\n",data);
+        //printf("Length of data : %d\n", strlen(data));
+
+        // Parse json string format data received from client end
+
         json_object * jobj = json_tokener_parse(data);
-        // Get the command value
         json_object * command = json_object_object_get(jobj,"command");
         printf("command is %s\n",json_object_get_string(command));
-        /* Receive message */
-       
+   
         // Command is postVideo
         if (strcmp(json_object_get_string(command),"postVideo") == 0)
         {
@@ -123,11 +136,11 @@ int main( int argc, char **argv)
         }
         
         // Command is postVideoTask
-     	if (strcmp(json_object_get_string(command),"postVideoTask") == 0)
-	{
+        if (strcmp(json_object_get_string(command),"postVideoTask") == 0)
+        {
             printf("Deal with postVideoTask \n");
-            init_task(jobj);		
-	}
+            init_task(jobj);        
+        }
            
         // Command is getVideoList
         if (strcmp(json_object_get_string(command),"getVideoList") == 0){
@@ -153,16 +166,24 @@ int main( int argc, char **argv)
                
             status = send(new_conn_fd,json_object_get_string(jobj),strlen(json_object_get_string(jobj)),0);
          }   
-	//status = send(new_conn_fd,"Welcome", 7,0);
-	if(status == -1)
-	{
-	    close(new_conn_fd);
-	    _exit(4);
-	}
+        
+        //status = send(new_conn_fd,"Welcome", 7,0);
+        if(status == -1)
+        {
+            close(new_conn_fd);
+            _exit(4);
+        }
 
-	}
-	// Close the socket before we finish
-	close(new_conn_fd);
-	return 0;
+    }
+    // Close the socket before we finish
+    close(new_conn_fd);
 }
+
+
+int main( int argc, char **argv)  
+{
+    init_tcp_server();
+    return 0;
+}
+
 
